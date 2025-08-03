d250422c63d7db60e9fd1362dc59bc43cfda1c99
Beka <fomapking@gmail.com>
2025-08-03 17:57:49 +0500

    Added ChunkDimentional travel

---
+++ Assets/Chunk.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the colliding object is the player
+        if (other.CompareTag("Player"))
+        {
+            // Make the player a child of the chunk when they enter
+
+            yield return null; 
+            other.transform.SetParent(transform);
+        }
+    }
+
+    IEnumerator OnTriggerExit2D(Collider2D other)
+    {
+        // Check if the object leaving is the player
+        if (other.CompareTag("Player"))
+        {
+            // Detach the player from the chunk when they exit
+            yield return null; 
+            other.transform.SetParent(null);
+        }
+    }
+
+}

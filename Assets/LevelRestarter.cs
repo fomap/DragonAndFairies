635d087b6e5c250d9bb4752001350b03954bbea2
Beka <fomapking@gmail.com>
2025-08-04 18:15:59 +0500

    Base components

---
+++ Assets/LevelRestarter.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestarter : MonoBehaviour
+{
+    [SerializeField] private float checkInterval = 0.5f;
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private string boxTag = "Box";
+    
+    private void Start()
+    {
+        StartCoroutine(CheckObjectsParent());
+    }
+
+    private IEnumerator CheckObjectsParent()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(checkInterval);
+            
+            // Check player
+            GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+            if (player != null && player.transform.parent == null)
+            {
+                RestartLevel();
+                yield break;
+            }
+            
+            // Check all boxes
+            GameObject[] boxes = GameObject.FindGameObjectsWithTag(boxTag);
+            foreach (var box in boxes)
+            {
+                if (box.transform.parent == null)
+                {
+                    RestartLevel();
+                    yield break;
+                }
+            }
+        }
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}

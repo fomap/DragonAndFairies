7a7bb66c2f1ea7df6b3cdb86aa1c7c4502dd3fda
Beka <fomapking@gmail.com>
2025-12-28 15:29:57 +0500

    added new dialogue entry structure populated dialogues

--- Assets/Scenes/Scripts/Dialogueold.cs
+++ Assets/Scenes/Scripts/Dialogueold.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Dialogue : MonoBehaviour
+public class Dialogueold : MonoBehaviour
 {
 
-    public static Dialogue Instance { get; private set; }
+    public static Dialogueold Instance { get; private set; }
 
     [SerializeField] private DialogueData dialogueData;
     private AsyncOperation asyncLoadOperation;
